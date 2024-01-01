b2e48140d6ee91b478aab2aef6cd58b7febf45c8
Prajwal <prajwalap1701@gmail.com>
2024-01-01 17:24:52 +0530

    Create instance

--- project-1/main.tf
+++ project-1/main.tf
@@ -3,9 +3,9 @@ resource "aws_vpc" "project-1-vpc" {
 }
 
 resource "aws_subnet" "subnet-1" {
-  vpc_id     = aws_vpc.project-1-vpc.id
-  cidr_block = "10.0.0.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.project-1-vpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -14,9 +14,9 @@ resource "aws_subnet" "subnet-1" {
 }
 
 resource "aws_subnet" "subnet-2" {
-  vpc_id     = aws_vpc.project-1-vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.project-1-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
   tags = {
@@ -45,4 +45,57 @@ resource "aws_route_table_association" "rta-1" {
 resource "aws_route_table_association" "rta-2" {
   subnet_id      = aws_subnet.subnet-2.id
   route_table_id = aws_route_table.project-1-RT.id
+}
+
+resource "aws_security_group" "project-1-sg" {
+  name        = "project-1-sg"
+  description = "Allow HTTP and SSH inbound traffic"
+  vpc_id      = aws_vpc.project-1-vpc.id
+
+  ingress {
+    description = "HTTP from Anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH from Anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-http-ssh"
+  }
+}
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "project-1-bucket-bw"
+}
+
+resource "aws_instance" "webserver1" {
+  ami                    = var.ec2_details.ami
+  instance_type          = var.ec2_details.type
+  vpc_security_group_ids = [aws_security_group.project-1-sg.id]
+  subnet_id              = aws_subnet.subnet-1.id
+  user_data              = base64encode(file("apache-v1.sh"))
+}
+
+resource "aws_instance" "webserver2" {
+  ami                    = var.ec2_details.ami
+  instance_type          = var.ec2_details.type
+  vpc_security_group_ids = [aws_security_group.project-1-sg.id]
+  subnet_id              = aws_subnet.subnet-2.id
+  user_data              = base64encode(file("apache-v2.sh"))
 }
--- project-1/variables.tf
+++ project-1/variables.tf
@@ -1,3 +1,12 @@
 variable "cidr_block" {
   default = "10.0.0.0/16"
 }
+
+variable "ec2_details" {
+  description = "Details of ec2 instance"
+  type        = map(string)
+  default = {
+    ami  = "ami-06aa3f7caf3a30282"
+    type = "t2.micro"
+  }
+}
